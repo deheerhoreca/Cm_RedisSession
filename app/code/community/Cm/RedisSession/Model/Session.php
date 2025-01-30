22769fed2858a1292ad191995dc1d46a33a0f1c6
Loek van Gool <loekvangool@gmail.com>
2025-01-30 23:20:59 +0100

    Update Session.php

--- app/code/community/Cm/RedisSession/Model/Session.php
+++ app/code/community/Cm/RedisSession/Model/Session.php
@@ -68,15 +68,17 @@ class Cm_RedisSession_Model_Session implements SessionHandlerInterface
      */
     public function setSaveHandler()
     {
-        session_set_save_handler(
-            array($this, 'open'),
-            array($this, 'close'),
-            array($this, 'read'),
-            array($this, 'write'),
-            array($this, 'destroy'),
-            array($this, 'gc')
-        );
-        return $this;
+        if(headers_sent() === false) {
+           session_set_save_handler(
+               array($this, 'open'),
+               array($this, 'close'),
+               array($this, 'read'),
+               array($this, 'write'),
+               array($this, 'destroy'),
+               array($this, 'gc')
+           );
+           return $this;
+        }
     }
 
     /**
